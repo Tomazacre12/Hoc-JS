272c1702a009b808348419408a3ad54fed8a62dd
Tomazacre12 <shadow12122004@gmail.com>
2022-08-23 22:23:01 +0700

    địa ngục gọi lại (kim tự tháp diệt vong)

---
+++ Lesson 89: Nỗi đau/main.js
@@ -0,0 +1,38 @@
+// Kim tự tháp diệt vong
+
+setTimeout(function(){
+    console.log(1)
+    setTimeout(function(){
+        console.log(2)
+        setTimeout(function(){
+            console.log(3)
+            setTimeout(function(){
+                console.log(4)
+                setTimeout(function(){
+                    console.log(5)
+                    setTimeout(function(){
+                        console.log(6)
+                        setTimeout(function(){
+                            console.log(7)
+                            setTimeout(function(){
+                                console.log(8)
+                                setTimeout(function(){
+                                    console.log(9)
+                                    setTimeout(function(){
+                                        console.log(10)
+                                        setTimeout(function(){
+                                            console.log(11)
+                                            setTimeout(function(){
+                                                console.log(12)
+                                            },1000)
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
