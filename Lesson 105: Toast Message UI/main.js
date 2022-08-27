5cf705c9555cbb8936795a8e43545060da3e947c
Tomazacre12 <shadow12122004@gmail.com>
2022-08-27 18:56:52 +0700

    add

--- Lesson 105: Toast Message UI/main.js
+++ Lesson 105: Toast Message UI/main.js
@@ -6,6 +6,15 @@ tittle = ''
     const main = document.getElementById('toast')
     if(main){
         const toast = document.createElement('div')
+        const autoRemoveId = setTimeout(function(){
+            main.removeChild(toast)
+        }, duration + 1000)
+        toast.onclick = function(e){
+            if (e.target.closest('.close')){
+                 main.removeChild(toast)
+                 clearTimeout(autoRemoveId)
+            }
+        }
         const icons = {
             Sucess: 'fa-solid fa-check-circle',
             Info: 'fa-solid fa-circle-info',
@@ -13,7 +22,10 @@ tittle = ''
             Error: 'fa-solid fa-circle-xmark',
         }
         const icon = icons[type]
+        const delay = (duration / 1000).toFixed(2)
         toast.classList.add('toast',`toast--${type}`)
+        toast.style.animation =`slideInLeft ease 0.5s, fadeOut linear 1s 3s forwards`
+
         toast.innerHTML =
         `<div class="icon"><i class="${icon}"></i></div>
             <div class="body">
